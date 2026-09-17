2c30c30f1992356474e9ff39ecf2a8baac9b0077
aadith <aadithnocky2@gmail.com>
2026-09-18 01:32:05 +0530

    Initial Commit

---
+++ PuffPact/PuffPactApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuffPactApp.swift
+//  PuffPact
+//
+//  Created by Aadith Bijith on 18/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct PuffPactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PuffPactTests/PuffPactTests.swift
@@ -0,0 +1,18 @@
+//
+//  PuffPactTests.swift
+//  PuffPactTests
+//
+//  Created by Aadith Bijith on 18/09/2026.
+//
+
+import Testing
+
+struct PuffPactTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
