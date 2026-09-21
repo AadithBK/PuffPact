3111fb11cd940b76ba81b0dab528606e64b27242
aadith <aadithnocky2@gmail.com>
2026-09-21 14:48:48 +0530

    feat: Integrate authentication flow and Firebase initialization into app lifecycle

--- PuffPact/PuffPactApp.swift
+++ PuffPact/PuffPactApp.swift
@@ -1,13 +1,44 @@
 import SwiftUI
+import FirebaseCore
 
 @main
 struct PuffPactApp: App {
+    @StateObject private var authService: FirebaseAuthService
     @StateObject private var appState = AppState()
+    @State private var showingSignUp = false
+    
+    init() {
+        FirebaseApp.configure()
+        _authService = StateObject(wrappedValue: FirebaseAuthService())
+    }
     
     var body: some Scene {
         WindowGroup {
-            DashboardView()
-                .environmentObject(appState)
+            Group {
+                switch authService.sessionState {
+                case .initializing:
+                    ProgressView("Connecting...")
+                    
+                case .unauthenticated:
+                    if showingSignUp {
+                        SignUpView(authService: authService) {
+                            showingSignUp = false
+                        }
+                    } else {
+                        LoginView(authService: authService) {
+                            showingSignUp = true
+                        }
+                    }
+                    
+                case .needsGroupOnboarding:
+                    OnboardingView(authService: authService)
+                    
+                case .authenticated:
+                    DashboardView()
+                        .environmentObject(appState)
+                }
+            }
+            .animation(.easeInOut, value: authService.sessionState != .unauthenticated)
         }
     }
 }
